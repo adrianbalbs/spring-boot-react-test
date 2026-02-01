63be62bc6811a54b27bee79bfb66eada89e6f1ab
Adrian Balbalosa <adrianbalbs@pm.me>
2026-02-01 19:30:38 +1100

    switch to using tanstack router, get routing working

---
+++ src/main/java/com/adrian/springbootreacttest/SpaWebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.adrian.springbootreacttest;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpaWebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/")
+                .setViewName("forward:/index.html");
+
+        registry.addViewController("/{path:[^\\.]*}")
+                .setViewName("forward:/index.html");
+
+        registry.addViewController("/**/{path:[^\\.]*}")
+                .setViewName("forward:/index.html");
+    }
+}
